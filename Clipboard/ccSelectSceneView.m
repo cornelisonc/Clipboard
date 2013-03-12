bd1129f802ebeac4309a78f2737308b1ec4a03d1
Ceili Cornelison <ceili@ceilicornelison.com>
2013-03-12 13:14:32 -0500

    Added selectSceneView

---
+++ Clipboard/ccSelectSceneView.m
@@ -0,0 +1,31 @@
+//
+//  ccSelectSceneView.m
+//  Clipboard
+//
+//  Created by Ceili Cornelison on 3/12/13.
+//  Copyright (c) 2013 muit. All rights reserved.
+//
+
+#import "ccSelectSceneView.h"
+
+@implementation ccSelectSceneView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
