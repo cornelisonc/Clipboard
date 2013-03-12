c3a52e2f4e1e530bc03b83b6d06c8d081a387295
Ceili Cornelison <ceili@ceilicornelison.com>
2013-03-12 13:18:49 -0500

    Added view controller for the scene selector view

---
+++ Clipboard/ccSelectSceneViewController.h
@@ -0,0 +1,13 @@
+//
+//  ccSelectSceneViewController.h
+//  Clipboard
+//
+//  Created by Ceili Cornelison on 3/12/13.
+//  Copyright (c) 2013 muit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ccSelectSceneViewController : UIViewController
+
+@end
